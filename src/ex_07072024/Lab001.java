2720022d5f42cc46d8925e44ea23ce72db4ef474
Arya <aryathampyan@gmail.com>
2024-07-16 09:33:18 +0530

    Added all files and folders from Pramod repo

--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -2,6 +2,6 @@ package ex_07072024;
 
 public class Lab001 {
     public static void main(String[] args) {
-        System.out.println("first class file");
+        System.out.println("Hello World!");
     }
 }
--- src/ex_07072024/Lab002.java
+++ src/ex_07072024/Lab002.java
@@ -2,6 +2,6 @@ package ex_07072024;
 
 public class Lab002 {
     public static void main(String[] args) {
-        System.out.println("second class to see commit and push");
+        System.out.println("Hello World Pramod!!");
     }
 }
